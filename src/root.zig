1a40b0a308fbe17228aca636bbcb1daa190f94de
IfkumRfnl <neto_frye@mail.ru>
2026-06-08 00:00:02 +0400

    Add VP8 boolean arithmetic writer

--- src/root.zig
+++ src/root.zig
@@ -17,6 +17,7 @@ pub const mux = @import("mux.zig");
 pub const options = @import("options.zig");
 pub const testing = @import("testing.zig");
 pub const vp8_bool_reader = @import("vp8/bool_reader.zig");
+pub const vp8_bool_writer = @import("vp8/bool_writer.zig");
 pub const vp8l_huffman = @import("vp8l/huffman.zig");
 
 pub const AnimationFrame = animation.Frame;
@@ -43,6 +44,7 @@ pub const MuxOptions = mux.Options;
 pub const ResourceLimits = limits.ResourceLimits;
 pub const StaticImage = mux.StaticImage;
 pub const VP8BoolReader = vp8_bool_reader.BoolReader;
+pub const VP8BoolWriter = vp8_bool_writer.BoolWriter;
 pub const VP8LCodeLengthHuffmanTable = vp8l_huffman.CodeLengthTable;
 pub const VP8LHuffmanSymbolTable = vp8l_huffman.SymbolTable;
 
